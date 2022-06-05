c040d38ea959f2b5f7c61061610159f9e6f9cc8c
Brian De Maio <bdemaio1@gmail.com>
2022-06-05 14:53:48 -0400

    NFT contract now has max supply and a minting price

--- contracts/NFT.sol
+++ contracts/NFT.sol
@@ -3,9 +3,14 @@ pragma solidity ^0.8.1;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
+import "@openzeppelin/contracts/security/PullPayment.sol";
 
-contract NFT is ERC721 {
+contract NFT is ERC721, PullPayment {
   using Counters for Counters.Counter;
+
+  // Constants
+  uint256 public constant TOTAL_SUPPLY = 10_000;
+  uint256 public constant MINT_PRICE = 0.08 ether;
 
   Counters.Counter private currentTokenId;
 
@@ -16,7 +21,11 @@ contract NFT is ERC721 {
     baseTokenURI = "";
   }
 
-  function mintTo(address recipient) public returns (uint256) {
+  function mintTo(address recipient) public payable returns (uint256) {
+    uint256 tokenId = currentTokenId.current();
+    require(tokenId < TOTAL_SUPPLY, "Max supply reached");
+    require(msg.value == MINT_PRICE, "Transaction value did not equal the mint price");
+
     currentTokenId.increment();
     uint256 newItemId = currentTokenId.current();
     _safeMint(recipient, newItemId);
